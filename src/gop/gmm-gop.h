ee77ef038670ae193ad33db827785e83ebbe30b9
jiangyangbo <75008724@qq.com>
2018-11-27 15:23:50 +0800

    Ver.20181126  添加Makefile 和 更新后的gop

--- src/gop/gmm-gop.h
+++ src/gop/gmm-gop.h
@@ -1,44 +1,63 @@
 // gop/gop-gmm.h
 
-// Copyright 2006  Junbo Zhang                    
+// Copyright 2016-2017  Junbo Zhang
 
-// This project based on Kaldi (https://github.com/kaldi-asr/kaldi).
-// All the Kaldi's codes in this project are under the Apache License, Version 2.0.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// 
-// However, this file has not been merged into Kaldi's master branch,
-// and the codes in this file are NOT UNDER THE SAME LICENSE of Kaldi's.
-// The codes in this file are NOT ALLOWED TO USE, COPY, DISTRIBUTE, OR MODIFY
-// unless being permitted by the author.
+// This program based on Kaldi (https://github.com/kaldi-asr/kaldi).
+// However, this program is NOT UNDER THE SAME LICENSE of Kaldi's.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 2 as published by the Free Software Foundation;
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
 #ifndef KALDI_GOP_GMM_H_
 #define KALDI_GOP_GMM_H_ 1
 
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
 #include "gmm/am-diag-gmm.h"
+#include "decoder/training-graph-compiler.h"
+#include "gmm/decodable-am-diag-gmm.h"
 #include "hmm/transition-model.h"
+#include "decoder/faster-decoder.h"
+#include "fstext/fstext-utils.h"
 
 namespace kaldi {
 
 class GmmGop {
- public:
-  GmmGop(AmDiagGmm &am, TransitionModel &tm) : am_(am), tm_(tm) {}
+public:
+  GmmGop() {}
+  void Init(std::string &tree_in_filename,
+            std::string &model_in_filename,
+            std::string &lex_in_filename);
+  void Compute(const Matrix<BaseFloat> &feats, const std::vector<int32> &transcript);
+  Vector<BaseFloat>& Result();
 
-  void Compute(const Matrix<BaseFloat> &feats,
-               const std::vector<int32> &align);
+protected:
+  AmDiagGmm am_;
+  TransitionModel tm_;
+  ContextDependency ctx_dep_;
+  TrainingGraphCompiler *gc_;
+  std::map<int32, int32> pdfid_to_tid;
+  Vector<BaseFloat> gop_result_;
 
-  void Write(std::ostream &os, bool binary) const;
-  void Read(std::istream &in, bool binary);
-
- protected:
-  AmDiagGmm &am_;
-  TransitionModel &tm_;
-
-  std::vector<BaseFloat> gop_result_;
-
-  BaseFloat ComputeGopNumera(SubMatrix<BaseFloat> &feats_in_phone,
-                             std::vector<int32> &align_in_phone);
-  BaseFloat ComputeGopDenomin(SubMatrix<BaseFloat> &feats_in_phone,
-                              std::vector<int32> &align_in_phone);
+  BaseFloat Decode(fst::VectorFst<fst::StdArc> &fst,
+                   DecodableAmDiagGmmScaled &decodable,
+                   std::vector<int32> *align = NULL);
+  BaseFloat ComputeGopNumera(DecodableAmDiagGmmScaled &decodable,
+                             std::vector<int32> &align,
+                             MatrixIndexT start_frame,
+                             int32 size);
+  BaseFloat ComputeGopNumeraViterbi(DecodableAmDiagGmmScaled &decodable,
+                                    int32 phone_l, int32 phone, int32 phone_r);
+  BaseFloat ComputeGopDenomin(DecodableAmDiagGmmScaled &decodable,
+                              int32 phone_l, int32 phone_r);
+  void GetContextFromSplit(std::vector<std::vector<int32> > split,
+                           int32 index, int32 &phone_l, int32 &phone, int32 &phone_r);
 };
 
 }  // End namespace kaldi
--- src/gopbin/compute-gmm-gop.cc
+++ src/gopbin/compute-gmm-gop.cc
@@ -1,15 +1,18 @@
 // gopbin/compute-gop-gmm.cc
 
-// Copyright 2006  Junbo Zhang                    
+// Copyright 2016-2017  Junbo Zhang
 
-// This project based on Kaldi (https://github.com/kaldi-asr/kaldi).
-// All the Kaldi's codes in this project are under the Apache License, Version 2.0.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// 
-// However, this file has not been merged into Kaldi's master branch,
-// and the codes in this file are NOT UNDER THE SAME LICENSE of Kaldi's.
-// The codes in this file are NOT ALLOWED TO USE, COPY, DISTRIBUTE, OR MODIFY
-// unless being permitted by the author.
+// This program based on Kaldi (https://github.com/kaldi-asr/kaldi).
+// However, this program is NOT UNDER THE SAME LICENSE of Kaldi's.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 2 as published by the Free Software Foundation;
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
 #include "base/kaldi-common.h"
 #include "util/common-utils.h"
@@ -18,61 +21,49 @@
 int main(int argc, char *argv[]) {
   try {
     using namespace kaldi;
-    typedef kaldi::int32 int32;
-    using fst::SymbolTable;
-    using fst::VectorFst;
-    using fst::StdArc;
     
     const char *usage =
-        "Compute GOP given [GMM-based] models.\n"
-        "Usage:   compute-gmm-gop [options] model-in feature-rspecifier alignments-rspecifier gop-out\n"
+        "Compute GOP with GMM-based models.\n"
+        "Usage:   compute-gmm-gop [options] tree-in model-in lexicon-fst-in feature-rspecifier transcriptions-rspecifier gop-wspecifier\n"
         "e.g.: \n"
-        " compute-gmm-gop 1.mdl scp:test.scp ark:1.ali 1.gop\n";
+        " compute-gmm-gop tree 1.mdl lex.fst scp:train.scp ark:train.tra ark,t:1.gop\n";
+
     ParseOptions po(usage);
+
     po.Read(argc, argv);
-    if (po.NumArgs() != 4) {
+    if (po.NumArgs() != 6) {
       po.PrintUsage();
       exit(1);
     }
 
-    std::string model_in_filename = po.GetArg(1);
-    std::string feature_rspecifier = po.GetArg(2);
-    std::string alignment_rspecifier = po.GetArg(3);
-    std::string gop_out_filename = po.GetArg(4);
+    std::string tree_in_filename = po.GetArg(1);
+    std::string model_in_filename = po.GetArg(2);
+    std::string lex_in_filename = po.GetArg(3);
+    std::string feature_rspecifier = po.GetArg(4);
+    std::string transcript_rspecifier = po.GetArg(5);
+    std::string gop_wspecifier = po.GetArg(6);
 
-    // Load resources
-    TransitionModel trans_model;
-    AmDiagGmm am_gmm;
-    {
-      bool binary;
-      Input ki(model_in_filename, &binary);
-      trans_model.Read(ki.Stream(), binary);
-      am_gmm.Read(ki.Stream(), binary);
-    }
     SequentialBaseFloatMatrixReader feature_reader(feature_rspecifier);
-    RandomAccessInt32VectorReader alignment_reader(alignment_rspecifier);
+    RandomAccessInt32VectorReader transcript_reader(transcript_rspecifier);
+    BaseFloatVectorWriter gop_writer(gop_wspecifier);
+
+    GmmGop gop;
+    gop.Init(tree_in_filename, model_in_filename, lex_in_filename);
 
     // Compute for each utterance
-    int32 num_done = 0, num_err = 0, num_retry = 0;
     for (; !feature_reader.Done(); feature_reader.Next()) {
       std::string utt = feature_reader.Key();
-      const Matrix<BaseFloat> &features = feature_reader.Value();
-      if (! alignment_reader.HasKey(utt)) {
+      if (! transcript_reader.HasKey(utt)) {
         KALDI_WARN << "Can not find alignment for utterance " << utt;        
-        num_err++;
         continue;
       }
-      const std::vector<int32> &alignment = alignment_reader.Value(utt);
+      const Matrix<BaseFloat> &features = feature_reader.Value();
+      const std::vector<int32> &transcript = transcript_reader.Value(utt);
 
-      GmmGop gop(am_gmm, trans_model);
-      gop.Compute(features, alignment);
-      WriteKaldiObject(gop, gop_out_filename, false);
+      gop.Compute(features, transcript);
+      gop_writer.Write(utt, gop.Result());
     }
-  
-
-    KALDI_LOG << "Retried " << num_retry << " out of "
-              << (num_done + num_err) << " utterances.";
-    KALDI_LOG << "Done " << num_done << ", errors on " << num_err;
+    KALDI_LOG << "Done.";
     return 0;
   } catch(const std::exception &e) {
     std::cerr << e.what();
